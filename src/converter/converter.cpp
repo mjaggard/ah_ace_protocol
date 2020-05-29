766d8cd33d528bef54634ff94ad705c1bbde4408
Mat Jaggard <github@jaggard.org.uk>
2020-05-29 13:29:38 +0000

    Convert data to UDP

--- src/converter/converter.cpp
+++ src/converter/converter.cpp
@@ -2,15 +2,20 @@
 #include <vector>
 #include <string>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <linux/if_ether.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
-#include <gst/gst.h>
-
-#define ACE_MAX_PACKET_LEN 239
+#include <map>
+#include <unistd.h>
+#include <netdb.h>
+#include <errno.h>
+#include "converter.h"
 
 using namespace std;
+
+int portNumberSetup = 17000;
 
 int main()
 {
@@ -26,11 +31,11 @@ int main()
     unsigned char *buffer = (unsigned char *)malloc(ACE_MAX_PACKET_LEN); //to receive data
     memset(buffer, 0, ACE_MAX_PACKET_LEN);
 
+    int discardCounter = 100;
+    std::map<std::array<unsigned char,ETH_ALEN>, int> mapOfPorts;
+
     while (true)
     {
-        struct sockaddr saddr;
-        int saddr_len = sizeof(saddr);
-
         //Receive a network packet and copy in to buffer
         int buflen = recv(sock_r, buffer, ACE_MAX_PACKET_LEN, 0);
         if (buflen < 0)
@@ -46,6 +51,82 @@ int main()
             continue;
         }
 
-        extractPacket(buffer);
+        std::array<unsigned char, ETH_ALEN> macAddress;
+        std::copy(std::begin(eth->h_source), std::end(eth->h_source), std::begin(macAddress));
+
+        if (discardCounter > 0)
+        {
+            mapOfPorts.insert(std::make_pair(macAddress, 0));
+            discardCounter--;
+            continue;
+        }
+        else if (discardCounter == 0)
+        {
+            std::map<std::array<unsigned char,ETH_ALEN>, int>::iterator it = mapOfPorts.begin();
+            while (it != mapOfPorts.end())
+            {
+                int portNumber = portNumberSetup;
+                portNumberSetup++;
+                it->second = openConnection(portNumber);
+            }
+        }
+
+        int fd = getConnection(macAddress, mapOfPorts);
+
+        int start = 14;
+        if (buflen == 239)
+        {
+            start = 18;
+        }
+        if (send(fd, buffer + start, buflen - start, 0) == -1)
+        {
+            printf("Error sending datagram");
+        }
     }
+}
+
+int getConnection(std::array<unsigned char, ETH_ALEN> macAddress, std::map<std::array<unsigned char,ETH_ALEN>, int> mapOfPorts)
+{
+    std::map<std::array<unsigned char,ETH_ALEN>, int>::iterator it = mapOfPorts.find(macAddress);
+    if (it == mapOfPorts.end())
+    {
+        //create a new port
+        int port = portNumberSetup;
+        portNumberSetup++;
+
+        int fd = openConnection(port);
+
+        mapOfPorts.insert(std::make_pair(macAddress, fd));
+        return fd;
+    }
+    return it->second;
+}
+
+int openConnection(int portNumber)
+{
+    char *port;
+    sprintf(port, "%d", portNumber);
+
+    struct addrinfo* res = 0;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = 0;
+    hints.ai_flags = AI_ADDRCONFIG;
+    int err = getaddrinfo(MULTICAST_LOCATION, port, &hints, &res);
+    
+    int fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (fd == -1)
+    {
+        printf("Error opening outbound socket\n");
+        exit(-1);
+    }
+    if (connect(fd, res->ai_addr, sizeof(res)) == -1)
+    {
+        printf("Error connecting outbound socket\n");
+        exit(-1);
+    }
+
+    return fd;
 }
